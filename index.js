628d1cd4ecd8c89e783332c8d4c2d8c8248baa25
Xjojen <614562018@qq.com>
2018-03-02 15:36:46 +0800

    添加数据库查询

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var chatbot = express();
 var http = require('http').Server(chatbot);
 var path = require('path');
 var io = require('socket.io')(http);
-// var fetch = require('fetch');
 const fetch = require('isomorphic-fetch');
 
 var publicPath = path.resolve(__dirname);
@@ -23,39 +22,44 @@ io.on('connection', function(socket){
   }) 
   socket.on('chatMessage', function(msg){
     // var msg = JSON.parse(message);
+    var exec = require('child_process').exec;
     var content = msg.mine.content;
+    content = content.replace(/[ | ]*\n/g,'\n'); //去除行尾空白
+    content = content.replace(/\n[\s| | ]*\r/g,'\n'); //去除多余空行
+    content = content.replace(/ /ig,'');//去掉 
+    content = content.replace(/^[\s　]+|[\s　]+$/g, "");//去掉全角半角空格
+    content = content.replace(/[\r\n]/g,""); //去掉回车换行
+    var user_id = msg.mine.id
     var To = msg.to;
-    var func = fetch('http://www.tuling123.com/openapi/api?key=2af335334aca4b49ae294620e449338d&info=' + content, {
-      method: 'POST',
-      type: 'cors'
-    }).then(function(response) {
-      return response.json()
-    }).then(function(detail) {
-        To.content = detail.text;
-	To.name = "Intelligent_Chat";
-        socket.emit('reply', To);
-        // layim.getMessage(To);
+    var msg_json = {
+      "message": {
+        "text": content
+      },
+      "userInfo": {
+        "userId": msg.mine.id
+      }
+    };
+    var answer_url = 'http://127.0.0.1:5000/get_answer';
+    let myHeaders = new Headers();
+    myHeaders.append('Content-Type', 'application/json');
+    let request = new Request(answer_url, {
+       method: 'POST', 
+       mode: 'cors',
+       body: JSON.stringify(msg_json),
+       headers:myHeaders
     });
+    fetch(request)
+      .then(function(response) {
+        return response.json();
+      }).then(function(data) {
+        console.log(data);
+        To.content = data['answer'];
+        To.name = "Intelligent_Chat";
+        socket.emit('reply', To);
+      });
   });
 });
 
-    // switch(ret.code) {
-    //   case "100000":
-    //   content = ret.text;
-    //   break;
-    //   case '200000':
-    //   break;
-    //   case "302000":
-    //   break;
-    //   case "308000":
-    //   break;
-    // }
-   
-    // To.username = "Intelligent_Chat";
-    // // To.type = 'friend';
-    // // To.id = -2;
-    // To.mine = false;
-    // To.content = ret.text;
 
 http.listen(8000, function(){
   console.log('listening on *: 8000');
